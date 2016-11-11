fc309fd0f0a3c9b312e4a475d7971889b8f5d937
Sandeep Agarwal <sandeep102@yahoo.com>
2016-11-11 02:41:53 -0500

    Matrix Inversion function completed... yay

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below are two functions... 
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+## It returns a list of 4 functions
+## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix.
+## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve 
+## the inverse from the cache.
 
-## Write a short comment describing this function
+## makeCacheMatrix will accept a matrix object as input and defines 4 functions in it
+## it returns 4 functions set, get, setinv and getinv to be called in another function defined below
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     m_inv <- NULL
+     set <- function(y) {
+          x <<- y
+          m_inv <<- NULL
+     }
+     get <- function() x
+     setinv <- function(mat_inv) m_inv <<- mat_inv
+     getinv <- function() m_inv
+     list(set = set, get = get,
+          setinv = setinv,
+          getinv = getinv)
+     
 }
 
 
-## Write a short comment describing this function
+## cacheSolve will accept the list object with 4 functions in it
+## it will check if the parent environment has the inverse value already present then it will display the cached value
+## otherwise it will calculatre the inverse of the matrix and store the value in the parent variable by calling setinv function
+## set function can be called from the console to set the new matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+     m_inv <- x$getinv()
+     if(!is.null(m_inv)) {
+          message("getting cached data")
+          return(m_inv)
+     }
+     data <- x$get()
+     m_inv <- solve(data)
+     x$setinv(m_inv)
+     m_inv
 }
